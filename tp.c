6cce007f10d106b26a2aaf6b1afec85c46d3f224
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-09-06 17:15:51 -0300

    Added basic client acceptance test

--- tp.c
+++ tp.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
+#include "socket.h"
 
 int main(int argc, char **argv) {
-  puts("hello world!");
+  if (argc != 3)
+    return 1;
+
+  const char *host = argv[1];
+  const char *port = argv[2];
+  int connection = Socket_Connect(host, port);
+  Socket_SendN(connection, 1, "G");
+  char buffer[722+5] = { 0 };
+  Socket_ReceiveN(connection, 722+4, buffer);
+  Socket_Close(connection);
+  puts(buffer +4);
 }
