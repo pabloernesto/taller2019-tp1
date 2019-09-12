f4d82e76b3524e499e47695cce405257e6912403
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2019-09-12 15:49:36 -0300

    Renamed test

--- test_tp_server_badmode.sh
+++ test_tp_server_badmode.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
-testname=tp_badmodes
+testname=test_tp_server_badmode
 source testing.sh
 
 tempfile stdout
+./tp server >$stdout
 
 tempfile exout
 echo -ne "Uso: ./tp server <puerto>\n" >$exout
-
-./tp server >$stdout
-
 delta=`diff $stdout $exout`
 if [ "$delta" != "" ]; then
   fail "expected `cat $exout` but got `cat $stdout`"
